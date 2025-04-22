86bb910335d71ee22add8a9e8033b22a8ba7a559
Noor A. S. <nalosag@gmail.com>
2025-04-22 10:09:20 +0200

    UPDATE (heroes component): Comienzo del tutorial de 0
    Al seleccionar en el nombre del heroe, aparecen sus datos debajo.

--- tour-of-heros/src/app/pages/heroes/heroes.component.ts
+++ tour-of-heros/src/app/pages/heroes/heroes.component.ts
@@ -2,25 +2,28 @@ import { Component, signal } from '@angular/core';
 import { Hero } from '../../interfaces/hero';
 import { UpperCasePipe } from '@angular/common';
 import { HEROES } from '../../data/heroes.data';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 @Component({
   selector: 'app-heroes',
-  imports: [],
+  imports: [UpperCasePipe, FormsModule],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
 
 export class HeroesComponent {
 
-  heroes = signal<Hero[]>(HEROES);
-
-
+  //heroes = signal<Hero[]>(HEROES);
 
   selectedHero?: Hero;
 
-  //ns para que es esto
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
 }
